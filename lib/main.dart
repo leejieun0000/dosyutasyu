6c87cea277e0ce3de022238bcdea58558ed06fb8
minsung <kms010218@gmail.com>
2024-05-11 16:20:26 +0900

    added current location feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'dart:math' as math;
 
 
 void main() {
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: const MyHomePage(),
     );
@@ -31,11 +33,15 @@ class _MyHomePageState extends State<MyHomePage> {
   late GoogleMapController mapController;
   final Location location = Location();
 
+  // 기본 카메라 위치
   CameraPosition currentPosition = CameraPosition(
-    // 기본 카메라 위치
       target: LatLng(37.555946, 126.972317),
       zoom : 14,
   );
+
+  bool isLoading = true;
+
+  Set<Marker> markers = {};
 
   @override
   void initState() {
@@ -49,6 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
     PermissionStatus _permissionGranted;
     LocationData _locationData;
 
+    // 위치 서비스 활성화 확인
     _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
       _serviceEnabled = await location.requestService();
@@ -57,6 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
+    // 위치 권한 확인
     _permissionGranted = await location.hasPermission();
     if (_permissionGranted == PermissionStatus.denied) {
       _permissionGranted = await location.requestPermission();
@@ -65,13 +73,47 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
+    // 현재 위치 얻기
     _locationData = await location.getLocation();
-    setState(() {
-      currentPosition = CameraPosition(
-        target: LatLng(_locationData.latitude!, _locationData.longitude!),
-        zoom: 14,
-      );
+
+    // 위치 정보의 변화를 실시간으로 감지하기 위한 리스너 설정
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      setState(() {
+        currentPosition = CameraPosition(
+          target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+          zoom: 14,
+        );
+
+        isLoading = false;
+
+        // 마커 추가
+        markers.add(
+          Marker(
+            markerId: MarkerId("currentLocation"),
+            position: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+            infoWindow: InfoWindow(
+              title: "현재 위치",
+            ),
+            // 사용자가 보고 있는 방향을 나타내는 화살표 아이콘 추가
+            // 여기서는 임시로 화살표 아이콘 대신 기본 아이콘을 회전시키는 방법을 사용
+            rotation: currentLocation.heading!, // 방향(헤딩)에 따라 마커 회전
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+          ),
+        );
+      });
     });
+  }
+
+  void _moveCameraToCurrentPosition() async {
+    final LocationData _locationData = await location.getLocation();
+    mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(_locationData.latitude!, _locationData.longitude!),
+          zoom: 14,
+        ),
+      ),
+    );
   }
 
   @override
@@ -80,13 +122,22 @@ class _MyHomePageState extends State<MyHomePage> {
        appBar: AppBar(
            title: Text("도슈타슈"),
      ),
-       body : Container(
-           child: GoogleMap(
-             initialCameraPosition: currentPosition,
-             onMapCreated: (GoogleMapController controller) {
-               mapController = controller;
-           }
-           )),
+       body: isLoading
+           ? Center(child: CircularProgressIndicator())
+           : Container(
+               child: GoogleMap(
+                 initialCameraPosition: currentPosition,
+                 markers: markers,
+                 onMapCreated: (GoogleMapController controller) {
+                   mapController = controller;
+                 },
+               ),
+           ),
+       floatingActionButton: FloatingActionButton(
+         onPressed: _moveCameraToCurrentPosition,
+         tooltip: '현재 위치로 이동',
+         child: const Icon(Icons.my_location),
+       ),
      );
   }
 }
